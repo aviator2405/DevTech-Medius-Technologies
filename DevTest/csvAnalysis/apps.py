55be9561551fd7e1dc8ead078aea08d052b6f675
aviator2405 <rp240502@gmail.com>
2024-11-10 12:17:21 +0530

    file change

--- DevTest/csvAnalysis/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CsvanalysisConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'csvAnalysis'
--- DevTest/csvAnalysis/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from csvAnalysis import views
-
-urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('',views.home,name="index"),   
-]
